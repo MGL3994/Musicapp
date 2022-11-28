48d80aa362ab29942e17182cb55bd41ad8982b64
Miguel <miguel.94@me.com>
2022-11-28 20:11:51 +0100

    Initial Commit

---
+++ Musica/MusicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicaApp.swift
+//  Musica
+//
+//  Created by Miguel on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
